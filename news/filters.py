05ee92a83f356a82207c1973021536b623c012bf
BOPOH1243 <tenpenni@mail.ru>
2022-10-19 00:03:37 +0500

    добавил удаление поста

--- news/filters.py
+++ news/filters.py
@@ -1,7 +1,7 @@
 from django_filters import FilterSet, ModelChoiceFilter, ModelMultipleChoiceFilter
 from .models import Post, Category
 from django_filters import *
-
+from django import forms
 class NewsFilter(FilterSet):
     category = ModelChoiceFilter(
         field_name = 'postcategory__category',
@@ -9,6 +9,13 @@ class NewsFilter(FilterSet):
         label='Category',
         #empty_label = 'any'
     )
+    #просто нашёл в интернете, вроде работает исправно
+    start_date = DateFilter(field_name='created_at',
+                            widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+                            lookup_expr='gt', label='Start Date')
+    end_date = DateFilter(field_name='created_at',
+                          widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+                          lookup_expr='lt', label='End Date')
     #created = DateTimeFilter()
 
     class Meta:
